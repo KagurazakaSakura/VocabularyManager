68d688e8138e3f046824c3f164df5a44b4e42937
Jzc <mytranser@outlook.com>
2017-06-22 22:46:16 +0800

    Update merriam_webster.py

--- merriam_webster.py
+++ merriam_webster.py
@@ -4,7 +4,7 @@ import re
 print("webster dictionary start")
 def request_xml(word):
     r = urllib.request.urlopen("http://www.dictionaryapi.com/api/v1/references/learners/xml/" + word + "?key=508b6e11-3920-41fe-a57a-d379deacf188").read().decode("utf-8")
-    print("gets success:"+word)
+    print("success->"+word)
     if len(r)<=83:
         return None
     else:
@@ -48,7 +48,4 @@ def get_all_defination(word_list):
                         yield entry
                         break
             else:
-                print("形式错误"+word)
-
-
-
+                print("unsuccess: "+word)
